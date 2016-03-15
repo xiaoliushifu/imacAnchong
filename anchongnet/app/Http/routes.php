957bc59aa6fb4dbfc1687e7cc96b39d2a9eafa98
redscarf <63160596@qq.com>
2016-03-15 23:13:20 +0800

    3-15添加最终后台版

--- anchongnet/app/Http/routes.php
+++ anchongnet/app/Http/routes.php
@@ -30,12 +30,15 @@
 Route::group(['domain' => 'admin.anchong.net'], function () {
      //首页路由
      Route::get('/','admin\indexController@index');
+     //视图下两层目录下的模版显示
      Route::get('/{path}/{path1}/{path2}',function($path,$path1,$path2){
          return view("admin.$path.$path1.".substr($path2,0,-10));
      });
+     //视图下一层目录下的模版显示
      Route::get('/{path}/{path1}',function($path,$path1){
          return view("admin.$path.".substr($path1,0,-10));
      });
+     //视图根目录下的模版显示
      Route::get('/{path}',function($path){
          return view("admin.".substr($path,0,-10));
      });
