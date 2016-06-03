ad3b128392364cff0b53ccf783b5c18e178446a8
fanjun <admin@fanxiao2.net>
2016-06-03 09:44:58 +0800

    提交几个冗余控制器

--- anchongnet/resources/views/inc/admin/mainHead.blade.php
+++ anchongnet/resources/views/inc/admin/mainHead.blade.php
@@ -4,7 +4,11 @@
 		<!-- mini logo for sidebar mini 50x50 pixels -->
 		<span class="logo-mini"><b>安</b>虫</span>
 		<!-- logo for regular state and mobile devices -->
-		<span class="logo-lg"><b>安虫</b>网</span>
+		@if(Auth::user()['user_rank']==3)
+		    <span class="logo-lg"><b>安虫</b>网</span>
+		@else
+			<span class="logo-lg"><b>安虫</b>商户</span>
+		@endif
 	</a>
 	<!-- Header Navbar: style can be found in header.less -->
 	<nav class="navbar navbar-static-top" role="navigation">
@@ -15,27 +19,6 @@
 
 		<div class="navbar-custom-menu">
 			<ul class="nav navbar-nav">
-				<!-- Notifications: style can be found in dropdown.less -->
-				<li class="dropdown notifications-menu">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<i class="fa fa-bell-o"></i>
-						<span class="label label-warning">30</span>
-					</a>
-					<ul class="dropdown-menu">
-						<li class="header">您有十个通知</li>
-						<li>
-							<!-- inner menu: contains the actual data -->
-							<ul class="menu">
-								<li>
-									<a href="#">
-										<i class="fa fa-users text-aqua"></i> 5条站内信
-									</a>
-								</li>
-							</ul>
-						</li>
-						<li class="footer"><a href="#">查看所有</a></li>
-					</ul>
-				</li>
 				<!-- User Account: style can be found in dropdown.less -->
 				@include('inc.admin.usermain')
 				<!-- Control Sidebar Toggle Button -->
