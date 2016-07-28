ef3cd599e019059f8f33ce66dfd5f959a9ccf979
liumingwei <872140945@qq.com>
2016-07-28 16:59:06 +0800

    货品搜索的优化--缓存

--- anchongnet/app/Http/Controllers/SearchController.php
+++ anchongnet/app/Http/Controllers/SearchController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
-
 use DB;
+use Cache;
+
+
 /**
 *
 *   根据输入的关键字 进行搜索处理
@@ -22,16 +23,25 @@ class SearchController extends Controller
         $q=$req->get('q');
         $ql=mb_strlen($q,'utf-8');
         if ($ql<1 || $ql>8) {
-            return response()->json(['search'=>$q,'fenci'=>'Too','ResultData'=>[]]);
+            return response()->json(['search'=>$q,'fenci'=>'Too','ResultData'=>['keyword too short']]);
         }
-        $tq = bin2hex($q);
-        //返回数组，但其元素是对象
-        $tmp=DB::select("select `cat_id` from `anchong_goods_keyword` where match(`keyword`) against(:key)",[$tq]);
-        foreach($tmp as $o) {
-            $tmparr[]= $o->cat_id;
+        //缓存前缀
+        $prefix='search';
+        //搜索缓存处理
+        if (!$result = Cache::get($prefix.$q)) {
+            $tq = bin2hex($q);
+            //返回数组，但其元素是对象
+            $tmp=DB::select("select `cat_id` from `anchong_goods_keyword` where match(`keyword`) against(:key)",[$tq]);
+            if (!$tmp) {
+                return response()->json(['search'=>$q,'fenci'=>'Too','ResultData'=>['Nothing to find']],200,['Content-type'=>'application/json;charset=utf-8'],JSON_UNESCAPED_UNICODE);
+            }
+            foreach($tmp as $o) {
+                $tmparr[]= $o->cat_id;
+            }
+            //去表中查找真正数据
+            $result = DB::table('anchong_goods_type')->whereIn('cat_id',$tmparr)->get();
+            Cache::add($prefix.$q,$result,60);
         }
-        //去表中查找真正数据
-        $result = DB::table('anchong_goods_type')->whereIn('cat_id',$tmparr)->get();
-        return response()->json(['search'=>$q,'fenci'=>'','ResultData'=>$result]);
+        return response()->json(['search'=>$q,'fenci'=>'','ResultData'=>$result],200,['Content-type'=>'application/json;charset=utf-8'],JSON_UNESCAPED_UNICODE);
     }
 }
