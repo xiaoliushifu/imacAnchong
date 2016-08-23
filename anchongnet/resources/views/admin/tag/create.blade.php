4517b85d58f116accdb47bb57025e0e86cba5570
liumingwei <872140945@qq.com>
2016-08-23 11:31:22 +0800

    商机标签浏览，名称更改，五大类商机标签b_type,大类下各种具体标签tag

--- anchongnet/resources/views/admin/tag/create.blade.php
+++ anchongnet/resources/views/admin/tag/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>添加标签</title>
+    <title>添加商机标签</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
@@ -45,7 +45,7 @@
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <h1>添加标签</h1>
+            <h1>添加商机标签</h1>
         </section>
         <!-- Main content -->
         <section class="content">
