2221ade18d4cab31c367abed74345b4c19b69026
liumingwei <872140945@qq.com>
2016-08-26 18:41:29 +0800

    服务端判定IE8

--- anchongnet/resources/views/inc/admin/footer.blade.php
+++ anchongnet/resources/views/inc/admin/footer.blade.php
@@ -5,10 +5,14 @@
 	<strong>Copyright &copy; 2016 <a href="http://www.anchong.net">安虫网</a>.</strong> All rights
 	reserved.
 </footer>
-<script src="/admin/plugins/jQuery/jQuery-2.2.0.min.js"></script>
-<!--[if lt IE 9]>
-<script src="/admin/plugins/jquery1/1.9.1/jquery.js"></script>
-<![endif]--> 
+{{--浏览器判定，只为兼容IE8--}}
+<?php  
+if (strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 8.0')) {
+    echo '<script src="/admin/plugins/jquery1/1.9.1/jquery.js"></script>';
+} else {
+    echo '<script src="/admin/plugins/jQuery/jQuery-2.2.0.min.js"></script>';
+}
+?>
 <script>
 	$(function(){
 		var activeFlag=$("#activeFlag").val();
